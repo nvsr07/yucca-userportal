06f3b9d0604f47f9bb97b85d27baa78ab62afc31
afranceschetti <alessandro.franceschetti@csi.it>
2015-05-28 09:59:07 +0200

    Version

--- userportal/src/main/java/org/csi/yucca/userportal/userportal/info/Info.java
+++ userportal/src/main/java/org/csi/yucca/userportal/userportal/info/Info.java
@@ -14,7 +14,7 @@ public class Info {
 	}
 
 	public Info() {
-		version = "1.1.0";
+		version = "1.2.0";
 	}
 
 	public User getUser() {
--- userportal/src/main/webapp/js/services/services.js
+++ userportal/src/main/webapp/js/services/services.js
@@ -4,7 +4,7 @@
 
 var appServices = angular.module('userportal.services', [ 'userportal.config' ]);
 
-appServices.value('version', '1.1.0');
+appServices.value('version', '1.2.0');
 
 appServices.factory('readFilePreview', function($q) {
 	return {
