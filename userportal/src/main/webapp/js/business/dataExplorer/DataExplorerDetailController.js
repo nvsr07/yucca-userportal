e8b26ba255a7e8e260af3646aac3d3be74e60c54
Davide Toppazzini <tophans@gmail.com>
2015-11-06 16:42:09 +0100

    fix

--- userportal/src/main/webapp/js/business/dataExplorer/DataExplorerDetailController.js
+++ userportal/src/main/webapp/js/business/dataExplorer/DataExplorerDetailController.js
@@ -37,7 +37,7 @@ appControllers.controller('DataExplorerDetailCtrl', [ '$scope', '$route', '$rout
 			$scope.VIRTUALENTITY_TYPE_TWITTER_ID = "Feed Tweet";
 			if($scope.dataset.tags!=null )
 				$scope.dataset.tagsArray = $scope.dataset.tags.split[","];
-			if (typeof($scope.dataset.tagsArray) == 'undefined'){
+			if ((typeof($scope.dataset.tagsArray) == 'undefined') && ($scope.dataset.tags != "")){
 				$scope.dataset.tagsArray = new Array();
 				$scope.dataset.tagsArray[0] = $scope.dataset.tags;
 			}
