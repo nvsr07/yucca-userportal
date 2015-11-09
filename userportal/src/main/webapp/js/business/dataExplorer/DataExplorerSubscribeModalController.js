3a3acbe8db9079686af5b3449f1b4aaa583fd4f6
afranceschetti <alessandro.franceschetti@csi.it>
2015-11-09 12:02:24 +0100

    dataexplorer subscription dialog

--- userportal/src/main/webapp/js/business/dataExplorer/DataExplorerSubscribeModalController.js
+++ userportal/src/main/webapp/js/business/dataExplorer/DataExplorerSubscribeModalController.js
@@ -1,13 +1,7 @@
-appControllers.controller('DataExplorerSubscribeModalCtrl', [ '$scope', '$routeParams','storeAPIservice', '$location', '$modalInstance', 'info', 
-                                                          function($scope, $routeParams, storeAPIservice, $location, $modalInstance, info ) {
-     	console.log('>>>>>>>> DataExplorerSubscribeModalController >>>>>>>> $scope', $scope);
-     	console.log('>>>>>>>> DataExplorerSubscribeModalController >>>>>>>> $routeParams', $routeParams);
-     	console.log('>>>>>>>> DataExplorerSubscribeModalController >>>>>>>> $location', $location);
-     	console.log('>>>>>>>> DataExplorerSubscribeModalController >>>>>>>> storeAPIservice', storeAPIservice);
-     	console.log('>>>>>>>> DataExplorerSubscribeModalController >>>>>>>> $modalInstance', $modalInstance);
-     	console.log('>>>>>>>> DataExplorerSubscribeModalController >>>>>>>> info', info);
-     	
-     	$scope.tenantCode = $routeParams.tenant_code;
+appControllers.controller('DataExplorerSubscribeModalCtrl', [ '$scope', '$routeParams','storeAPIservice', '$location', '$modalInstance', 'info', 'dataset',
+                                                          function($scope, $routeParams, storeAPIservice, $location, $modalInstance, info, dataset ) {
+     	console.log('DataExplorerSubscribeModalCtrl  dataset', dataset);
+
      	
      	$scope.cancel = function() {
      		$modalInstance.dismiss();
@@ -19,20 +13,80 @@ appControllers.controller('DataExplorerSubscribeModalCtrl', [ '$scope', '$routeP
 
      	
     	$scope.applicationList = null;
-    	$scope.subscriptionList = null;
+    	var subscriptionList = [];
+    	$scope.editedDescriptions =  [];
+    	
+    	
+    	var isApplicationSubscribed  = function(appName){
+    		var found = false;
+    		for (var subscriptionIndex = 0; subscriptionIndex < subscriptionList.length; subscriptionIndex++) {
+    			if(subscriptionList[subscriptionIndex].name == appName){
+    				found = true;
+    				break;
+    			}
+			}
+    		return found;
+    	};
 
-    	storeAPIservice.getApplications().success(function(response) {
-    		console.log("response",response);
-    		$scope.applicationList = response.applications;
+    	var loadApplications  = function(){
+    		storeAPIservice.getApplications().success(function(response) {
+	    		console.log("response",response);
+	    		$scope.applicationList = response.applications;
+	        	storeAPIservice.getSubscriptions().success(function(response) {
+	        		console.log("getSubscriptions response",response);
+	        		subscriptionList = response.subscriptions;
+	        		if($scope.applicationList!=null){
+	        			for (var appIndex = 0; appIndex < $scope.applicationList.length; appIndex++) {
+							$scope.applicationList[appIndex].isBusy = false;
+							$scope.applicationList[appIndex].isEditing = false;
+							$scope.applicationList[appIndex].isSubscribed = isApplicationSubscribed($scope.applicationList[appIndex].name);
+							$scope.editedDescriptions[appIndex] = $scope.applicationList[appIndex].description;
+							
+	        			}
+	        		}
+	        	});
+	    	});
+    	};
+    	
+    	
+    	loadApplications();
+    	
+    	 
+    	
+    	
+    	
+    	
+    	$scope.startEditApplication  = function(index){
+    		$scope.applicationList[index].isEditing = true;
+    	};
 
-    	});
+    	$scope.saveEditApplication  = function(index){
+    		$scope.applicationList[index].description = $scope.editedDescriptions[index];
+    		console.log("saveEditApplication: editedApplication",$scope.applicationList[index]);
+    		$scope.applicationList[index].isEditing = false;
+    	};
 
-    	storeAPIservice.getSubscriptions().success(function(response) {
-    		console.log("response",response);
-    		$scope.subscriptionList = response.subscriptions;
+    	$scope.cancelEditApplication  = function(index){
+    		$scope.editedDescriptions[index] = $scope.applicationList[index].description;
+    		$scope.applicationList[index].isEditing = false;
+    	};
+    	
+    	$scope.unsubscribeApplication = function(index){
+    		$scope.applicationList[index].isSubscribed = false;
+    	};
 
-    	});
+    	$scope.subscribeApplication = function(index){
+    		$scope.applicationList[index].isSubscribed = true;
+    	};
+
     	
     	
      }]);
 
+
+translations_it["APPLICATIONS_NAME"] = "Nome";
+translations_it["APPLICATIONS_DESCRIPTION"] = "Descrizione";
+
+translations_it["DATA_EXPLORER_SUBSCRIBE_NEW_APPLICATION_TITLE"] = "Crea nuova applicazione";
+translations_it["DATA_EXPLORER_SUBSCRIBE_ADD_SUBSCRIPTION"] = "Sottoscrivi";
+translations_it["DATA_EXPLORER_SUBSCRIBE_REMOVE_SUBSCRIPTION"] = "Annulla Sottoscrizione";
--- userportal/src/main/webapp/js/services/storeServices.js
+++ userportal/src/main/webapp/js/services/storeServices.js
@@ -1,23 +1,38 @@
-appServices.factory('storeAPIservice',["$http","$q","info", function($http, $q,info) {
+appServices.factory('storeAPIservice',["$http","$q","info", "$location", function($http, $q,info, $location) {
 
 	var storeAPI = {};
+	
+	var host = "";
+
+	getStoreBaseUrl = function(){
+		var host = $location.host();
+		if(host == 'localhost')
+			baseUrl = 'https://int-userportal.smartdatanet.it';
+		else{
+			var env = host.substring(0,host.indexOf("userportal.smartdatanet.it"));
+			baseUrl = 'https://'+env+'userportal.smartdatanet.it';
+		}
+		return baseUrl;
+	}
+	
 
 	storeAPI.getApplications = function() {
 		return $http({
-			method : 'JSONP',
-			url : 'https://int-userportal.smartdatanet.it/store/site/blocks/application/application-list/ajax/application-list.jag?action=getApplications',
+			method : 'GET',
+			url : getStoreBaseUrl()+'/store/site/blocks/application/application-list/ajax/application-list.jag?action=getApplications',
 			responseType :'json'
 		});
 	};
 
 	storeAPI.getSubscriptions = function() {
 		return $http({
-			method : 'JSONP',
-			url : 'https://int-userportal.smartdatanet.it/store/site/blocks/subscription/subscription-list/ajax/subscription-list.jag?action=getAllSubscriptions',
+			method : 'GET',
+			//url : 'https://int-userportal.smartdatanet.it/store/site/blocks/subscription/subscription-list/ajax/subscription-list.jag?action=getAllSubscriptions',
+			url : getStoreBaseUrl()+'/store/site/blocks/subscription/subscription-list/ajax/subscription-list.jag?action=getAllSubscriptions&application=s',
 			responseType :'json'
 		});
 		
-	}
+	};
 	
 	return storeAPI;
 }]);
